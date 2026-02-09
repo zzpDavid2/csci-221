c855b0262a34e7ebd652c231e1525ace6cdb06bb
zzpDavid2 <51421093+zzpDavid2@users.noreply.github.com>
2026-02-09 13:08:29 -0800

    Make string copy more primative

--- intro_c/evaluation/weekday.c
+++ intro_c/evaluation/weekday.c
@@ -4,41 +4,47 @@
 #define DEBUG 1
 
 char* weekday(char* charecters, enum day numerical) {
-    const char* day_str = NULL;
+    char* day_str = NULL;
+    size_t str_length = 0;
     
     switch (numerical) {
         case SUNDAY:
             day_str = "SUNDAY";
+            str_length = 6;
             break;
         case MONDAY:
             day_str = "MONDAY";
+            str_length = 6;
             break;
         case TUESDAY:
             day_str = "TUESDAY";
+            str_length = 7;
             break;
         case WEDNESDAY:
             day_str = "WEDNESDAY";
+            str_length = 9;
             break;
         case THURSDAY:
             day_str = "THURSDAY";
+            str_length = 8;
             break;
         case FRIDAY:
             day_str = "FRIDAY";
+            str_length = 6;
             break;
         case SATURDAY:
             day_str = "SATURDAY";
+            str_length = 8;
             break;
         default:
             day_str = "INVALID";
+            str_length = 7;
     }
     
     // Copy string character by character
-    int i = 0;
-    while (day_str[i] != '\0') {
+    for (uint8_t i = 0; i < str_length; i++) {
         charecters[i] = day_str[i];
-        i++;
     }
-    charecters[i] = '\0';  // null terminate
 
     #if DEBUG == 1
     printf("Numerical input: %d => Day: %s\n", numerical, charecters);
